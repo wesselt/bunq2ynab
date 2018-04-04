f2d39db7aeab2539c11e0001b531b3ebf904c545
wesselt <wesselt@aule.net>
2018-04-04 14:06:15 +0200

    Refactored ynab common functions

--- ynab.py
+++ ynab.py
@@ -4,6 +4,7 @@ import requests
 
 
 url = 'https://api.youneedabudget.com/'
+personal_access_token_file = "personal_access_token.txt"
 
 
 def read_file(fname):
@@ -15,37 +16,33 @@ def read_file(fname):
 
 
 def get_personal_access_token():
-    token = read_file("personal_access_token.txt")
-    if not token:
-        raise Exception("Couldn't read YNAB personal access token.  Get one " +
-            "from YNAB's developer settings and store it in " +
-            "personal_access_token.txt.")
-    return token.rstrip("\n")
+    token = read_file(personal_access_token_file)
+    if token:
+        return token.rstrip("\n")
+    raise Exception("Couldn't read YNAB personal access token.  Get one " +
+        "from YNAB's developer settings and store it in " +
+        personal_access_token_file)
 
 
-def default_headers():
-    return {
+def call(action, method, data = None):
+    headers =  {
         'Authorization': 'Bearer ' + get_personal_access_token(),
         'Content-type': 'application/json'
     }
+    if action == 'GET':
+        reply = requests.get(url + method, headers=headers)
+    elif action == 'POST':
+        reply = requests.post(url + method, headers=headers, data=data)
+    result = reply.json()
+    if "error" in result:
+        raise Exception("{0} (details: {1})".format(
+                           result["error"]["name"], result["error"]["detail"]))
+    return result["data"]
 
 
 def get(method):
-    headers = default_headers()
-    reply = requests.get(url + method, headers=headers)
-    result = reply.json()
-    if "error" in result:
-        raise Exception("{0} (details: {1})".format(
-                           result["error"]["name"], result["error"]["detail"]))
-    return result["data"]
+    return call('GET', method)
 
 
-def post(method, data_obj):
-    data = json.dumps(data_obj)
-    headers = default_headers()
-    reply = requests.post(url + method, headers=headers, data=data)
-    result = reply.json()
-    if "error" in result:
-        raise Exception("{0} (details: {1})".format(
-                           result["error"]["name"], result["error"]["detail"]))
-    return result["data"]
+def post(method, data):
+    return call('POST', method, data)
