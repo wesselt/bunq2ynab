0c95fdea5cb8bc9a536f41a3c9a27aa83f2d0132
wesselt <wesselt@aule.net>
2018-04-07 19:12:45 +0200

    Print YNAB request and reply

--- ynab.py
+++ ynab.py
@@ -6,6 +6,11 @@ import requests
 url = 'https://api.youneedabudget.com/'
 personal_access_token_file = "personal_access_token.txt"
 
+# 1 to log http calls, 2 to include headers
+log_level = 0
+
+
+# -----------------------------------------------------------------------------
 
 def read_file(fname):
     if os.path.isfile(fname):
@@ -23,21 +28,59 @@ def get_personal_access_token():
         personal_access_token_file)
 
 
+def log_request(action, method, headers, data):
+    if log_level < 1:
+        return
+    print("******************************")
+    print("{0} {1}".format(action, method))
+    if log_level > 1:
+        for k, v in headers.items():
+            print("  {0}: {1}".format(k, v))
+    if data:
+        print("-----")
+        print(json.dumps(data, indent=2))
+        print("-----")
+
+
+def log_reply(reply):
+    if log_level < 1:
+        return
+    print("Status: {0}".format(reply.status_code))
+    if log_level > 1:
+        for k, v in reply.headers.items():
+            print("  {0}: {1}".format(k, v))
+    print("----------")
+    if reply.headers["Content-Type"] == "application/json":
+        print(json.dumps(reply.json(), indent=2))
+    else:
+        print(reply.text)
+    print("******************************")
+
+
 def call(action, method, data_obj=None):
+    data = json.dumps(data_obj) if data_obj else ''
     headers = {
         'Authorization': 'Bearer ' + get_personal_access_token(),
         'Content-type': 'application/json'
     }
+    log_request(action, method, headers, data)
     if action == 'GET':
         reply = requests.get(url + method, headers=headers)
     elif action == 'POST':
-        data = json.dumps(data_obj)
         reply = requests.post(url + method, headers=headers, data=data)
+    log_reply(reply)
     result = reply.json()
     if "error" in result:
         raise Exception("{0} (details: {1})".format(
                            result["error"]["name"], result["error"]["detail"]))
     return result["data"]
+
+
+# -----------------------------------------------------------------------------
+
+def set_log_level(level):
+    global log_level
+    log_level = level
 
 
 def get(method):
