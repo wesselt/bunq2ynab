8ec88f34d55b263c52b4619b0cf42bdc745be746
wesselt <wesselt@aule.net>
2018-04-10 22:17:17 +0200

    Show bunq user and account ids

--- list_user.py
+++ list_user.py
@@ -7,14 +7,15 @@ import bunq
 def print_accounts(userid):
     method = 'v1/user/{0}/monetary-account'.format(userid)
     for a in [a["MonetaryAccountBank"] for a in bunq.get(method)]:
-        print("  {0:28}  {1:10,} {2}".format(
+        print("  {0:28}  {1:10,} {2:3}  ({3})".format(
             a["description"],
             Decimal(a["balance"]["value"]),
-            a["balance"]["currency"]))
+            a["balance"]["currency"],
+            a["id"]))
 
 
 users = bunq.get('v1/user')
 for u in users:
     for k, v in u.items():
-        print('{0} "{1}"'.format(k, v["display_name"]))
+        print('{0} "{1}" ({2})'.format(k, v["display_name"], v["id"]))
         print_accounts(v["id"])
