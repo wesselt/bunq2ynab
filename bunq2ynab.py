d1ddabc239ccbcd1a002389c4f7c675ac8ccd1fa
wesselt <wesselt@aule.net>
2018-05-09 22:39:14 +0200

    Improved YNAB duplicate handling
    Calculate occurence for multiple transactions with the same amount
    on the same day.  Occurrence is the third part of the "import_id" used
    by YNAB for duplicate detection.
    
    The calculation relies on having all transactions for a day.  To ensure
    correctness, skip uploading all transactions from the first day in the list.

--- bunq2ynab.py
+++ bunq2ynab.py
@@ -15,7 +15,7 @@ bunq_user_id = bunq.get_user_id(bunq_user_name)
 bunq_account_id = bunq.get_account_id(bunq_user_id, bunq_account_name)
 
 print("Reading list of payments...")
-method = ("v1/user/{0}/monetary-account/{1}/payment?count=24"
+method = ("v1/user/{0}/monetary-account/{1}/payment?count=100"
           .format(bunq_user_id, bunq_account_id))
 payments = bunq.get(method)
 
@@ -25,15 +25,24 @@ ynab_account_id = ynab.get_account_id(ynab_budget_id, ynab_account_name)
 
 print("Translating payments...")
 transactions = []
-for p in [p["Payment"] for p in payments]:
-    amount = p["amount"]["value"]
+first_day = None
+unsorted_payments = [p["Payment"] for p in payments]
+payments = sorted(unsorted_payments, key=lambda p: p["created"])
+for p in payments:
+    amount_value = p["amount"]["value"]
     if p["amount"]["currency"] != "EUR":
         raise Exception("Non-euro payment: " + p["amount"]["currency"])
     date = p["created"][:10]
+    if not first_day or date < first_day:
+        first_day = date
     payee = p["counterparty_alias"]["display_name"]
     description = p["description"]
 
-    milliunits = str((1000 * Decimal(amount)).quantize(1))
+    milliunits = str((1000 * Decimal(amount_value)).quantize(1))
+    # For YNAB duplicate detection
+    occurrence = 1 + len([t for t in transactions 
+                          if t["amount"] == milliunits and t["date"] == date])
+
     transactions.append({
         "account_id": ynab_account_id,
         "date": date,
@@ -41,12 +50,16 @@ for p in [p["Payment"] for p in payments]:
         "payee_name": payee[:50],  # YNAB payee is max 50 chars
         "memo": description[:100],  # YNAB memo is max 100 chars
         "cleared": "cleared",
-        "import_id": "YNAB:{0}:{1}:1".format(milliunits, date)
+        "import_id": "YNAB:{0}:{1}:{2}".format(milliunits, date, occurrence)
     })
+
+# Occurance calculation is not accurate for oldest day in the list,
+# so do not upload those transactions to YNAB
+safe_transactions = [t for t in transactions if first_day < t["date"]]
 
 print("Uploading transactions to YNAB...")
 method = "v1/budgets/" + ynab_budget_id + "/transactions/bulk"
-result = ynab.post(method, {"transactions": transactions})
+result = ynab.post(method, {"transactions": safe_transactions})
 stats = result["bulk"]
 print("Uploaded {0} new and {1} duplicate transactions.".format(
       len(stats["transaction_ids"]), len(stats["duplicate_import_ids"])))
