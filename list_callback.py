da7010f3d762b4b44078c12f41a03fd28cefa492
wesselt <wesselt@aule.net>
2018-05-23 16:12:36 +0200

    List callbacks for accounts
    in addition to callbacks for users

--- list_callback.py
+++ list_callback.py
@@ -15,12 +15,25 @@ log_level = 2 if args.vv else 1 if args.v else 0
 bunq.set_log_level(log_level)
 
 
+def print_notification_filter(e):
+    nfs = e["notification_filters"]
+    if not nfs:
+        print("  No callbacks")
+        return
+    for nf in e["notification_filters"]: 
+        print('  {0:35} {1:10} {2}'.format(
+            nf["category"],
+            nf["notification_delivery_method"],
+            nf.get("notification_target", "-")))
+
+
 users = bunq.get("v1/user")
 for u in users:
     for k, v in u.items():
-        print('{0} "{1}"'.format(k, v["display_name"]))
-        for nf in v["notification_filters"]:
-            print('  {0:35} {1:10} {2}'.format(
-              nf["category"],
-              nf["notification_delivery_method"],
-              nf.get("notification_target", "-")))
+        print('{0} "{1}":'.format(k, v["display_name"]))
+        print_notification_filter(v)
+
+        method = 'v1/user/{0}/monetary-account'.format(v["id"])
+        for a in [a["MonetaryAccountBank"] for a in bunq.get(method)]:
+            print('{} "{}" "{}":'.format(k, v["display_name"], a["description"]))
+            print_notification_filter(a)
