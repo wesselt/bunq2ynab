220ce6a333863f77ef0bdc47ddd38a5fb457c0c6
julius <fynh@posteo.org>
2019-03-15 10:59:34 +0100

    Added callback support

--- bunq_api.py
+++ bunq_api.py
@@ -24,16 +24,18 @@ def get_account_id(user_id, account_name):
 
 
 def get_callbacks(user_id, account_id):
-   method = 'v1/user/{0}/monetary-account/{1}'.format(user_id, account_id)
-   result = bunq.get(method)
-   return result[0]["MonetaryAccountBank"]["notification_filters"]
+    account_type = get_account_type(user_id, account_id)
+    method = 'v1/user/{0}/monetary-account/{1}'.format(user_id, account_id)
+    result = bunq.get(method)
+    return result[0][account_type]["notification_filters"]
 
 
 def put_callbacks(user_id, account_id, new_notifications):
+    account_type = get_account_type(user_id, account_id)
     data = {
          "notification_filters": new_notifications
     }
-    method = 'v1/user/{0}/monetary-account-bank/{1}'.format(
+    method = 'v1/user/{0}/' + bunq.get_account_path(account_type) + '/{1}'.format(
                                                            user_id, account_id)
     bunq.put(method, data)
 
