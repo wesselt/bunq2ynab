18f258224e7dbca5d3f64feb65c8d7d16513c3c2
wesselt <wesselt@aule.net>
2019-10-27 17:46:15 +0100

    Update list_callbacks to new endpoints

--- list_callback.py
+++ list_callback.py
@@ -3,6 +3,7 @@ import json
 import sys
 
 import bunq
+import bunq_api
 
 
 parser = argparse.ArgumentParser()
@@ -10,32 +11,38 @@ parser.add_argument("-v", action="store_true",
     help="Show content of JSON messages")
 parser.add_argument("-vv", action="store_true",
     help="Show JSON messages and HTTP headers")
+parser.add_argument("bunq_user_name",
+    help="Bunq user name (retrieve using 'python3 list_user.py')")
 args = parser.parse_args()
 log_level = 2 if args.vv else 1 if args.v else 0
 bunq.set_log_level(log_level)
 
 
-def print_notification_filter(e):
-    nfs = e["notification_filters"]
+def print_notification_filter(nfs):
     if not nfs:
         print("  No callbacks")
         return
-    for nf in nfs:
-        print('  {0:35} {1:10} {2}'.format(
+    for nfi in nfs:
+        nf = nfi["NotificationFilterUrl"]
+        print('  {} -> {}'.format(
             nf["category"],
-            nf["notification_delivery_method"],
             nf.get("notification_target", "-")))
 
 
-users = bunq.get("v1/user")
-for u in users:
-    for k, v in u.items():
-        print('{0} "{1}":'.format(k, v["display_name"]))
-        print_notification_filter(v)
+bunq_user_id = bunq_api.get_user_id(args.bunq_user_name)
 
-        method = 'v1/user/{0}/monetary-account'.format(v["id"])
-        for a in bunq.get(method):
-            for ac in a.values():
-                print('{} "{}" "{}":'.format(k, v["display_name"],
-                                             ac["description"]))
-                print_notification_filter(ac)
+method = f"v1/user/{bunq_user_id}/notification-filter-url"
+nfs = bunq.get(method)
+print("Callbacks for user:")
+print_notification_filter(nfs)
+
+# Loop over accounts for this user
+method = f"v1/user/{bunq_user_id}/monetary-account"
+for acs in bunq.get(method):
+    for ac in acs.values():
+        account_id = ac["id"]
+        print(f'Callbacks for account {account_id} "{ac["description"]}":')
+        method = (f"v1/user/{bunq_user_id}/monetary-account/{account_id}/" +
+                   "notification-filter-url")
+        nfs = bunq.get(method)
+        print_notification_filter(nfs)
