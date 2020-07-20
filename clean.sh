caa0dafb39c966b49a354b671f01838910e46b57
wesselt <wesselt@aule.net>
2020-07-20 19:01:01 +0200

    No more server public keys

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Removing BUNQ state files...
-rm private_key.txt installation_token.txt server_public.txt session_token.txt
+rm private_key.txt installation_token.txt session_token.txt
