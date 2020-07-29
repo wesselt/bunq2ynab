da4e8d5249696d01679c0c9a057a5b833f1752b8
wesselt <wesselt@aule.net>
2020-07-29 20:46:56 +0200

    Bugfix manual sync

--- bunq2ynab.py
+++ bunq2ynab.py
@@ -26,12 +26,12 @@ bunq.set_log_level(log_level)
 ynab.set_log_level(log_level)
 
 print("Getting ynab identifiers...")
-ynab_budget_id = ynab.get_budget_id(ynab_budget_name)
-ynab_account_id = ynab.get_account_id(ynab_budget_id, ynab_account_name)
+ynab_budget_id = ynab.get_budget_id(args.ynab_budget_name)
+ynab_account_id = ynab.get_account_id(ynab_budget_id, args.ynab_account_name)
 
 print("Getting bunq identifiers...")
-bunq_user_id = bunq_api.get_user_id(bunq_user_name)
-bunq_account_id = bunq_api.get_account_id(bunq_user_id, bunq_account_name)
+bunq_user_id = bunq_api.get_user_id(args.bunq_user_name)
+bunq_account_id = bunq_api.get_account_id(bunq_user_id, args.bunq_account_name)
 
 sync.synchronize(bunq_user_id, bunq_account_id,
                  ynab_budget_id, ynab_account_id)
