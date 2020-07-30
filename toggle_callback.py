ea092df6c8719490575a5fd691abec1ea9345349
wesselt <wesselt@aule.net>
2020-07-30 22:40:32 +0200

    Switch to wildcard IP by default

--- toggle_callback.py
+++ toggle_callback.py
@@ -11,6 +11,9 @@ parser.add_argument("-v", action="store_true",
     help="Show content of JSON messages")
 parser.add_argument("-vv", action="store_true",
     help="Show JSON messages and HTTP headers")
+parser.add_argument("--single-ip", action="store_true",
+    help="Register BUNQ device-server with a single IP address instead " +
+         "of a wildcard for all IPs.  Useful if you have a fixed IP.")
 parser.add_argument("bunq_user_name",
     help="Bunq user name (retrieve using 'python3 list_user.py')")
 parser.add_argument("bunq_account_name", nargs='?',
@@ -22,6 +25,7 @@ parser.add_argument("toggle_url",
 args = parser.parse_args()
 log_level = 2 if args.vv else 1 if args.v else 0
 bunq.set_log_level(log_level)
+bunq.set_single_ip(args.single_ip)
 
 
 def update_notifications(nfs):
