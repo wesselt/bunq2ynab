d72166c459f485f1eac1a0363bca018d9af62416
wesselt <wesselt@aule.net>
2020-08-07 23:34:18 +0200

    Added AWS lambda handler

---
+++ aws_lambda.py
@@ -0,0 +1,13 @@
+import sys
+
+from lib.sync import Sync
+from lib.config import config
+
+
+config.load()
+
+
+def handler(event, context):
+    sync = Sync()
+    sync.populate()
+    sync.synchronize()
