38607aefd4b9246ce23fe271f2aa062c3dc3ac9a
Javy de Koning <javy@amazon.com>
2020-08-13 09:39:58 +0200

    AWS Lambda compatibility: SAM Template, Support storing config in SSM, updated Logging to include functionname

---
+++ lib/parameter_store.py
@@ -0,0 +1,46 @@
+import boto3
+
+from lib.errors import ParameterNotFoundError
+from lib.log import log
+
+
+class ParameterStore:
+    """Class used for modeling Parameters
+    """
+
+    def __init__(self):
+        self.client = boto3.client('ssm')
+
+    def fetch_parameter(self, name, with_decryption=False):
+        """Gets a Parameter from Parameter Store (Returns the Value)
+        """
+        try:
+            log.debug('Fetching Parameter %s', name)
+            response = self.client.get_parameter(
+                Name=name,
+                WithDecryption=with_decryption
+            )
+            return response['Parameter']['Value']
+        except self.client.exceptions.ParameterNotFound:
+            errmsg = 'Parameter {0} Not Found'.format(name)
+            log.critical(errmsg)
+            raise ParameterNotFoundError(
+                errmsg
+            )
+
+    def put_parameter(self, name, value):
+        """Puts a Parameter into Parameter Store
+        """
+        try:
+            current_value = self.fetch_parameter(name)
+            assert current_value == value
+            log.debug(
+                'No need to update parameter %s with value %s since they are the same', name, value)
+        except (ParameterNotFoundError, AssertionError):
+            log.debug('Putting SSM Parameter %s with value %s', name, value)
+            self.client.put_parameter(
+                Name=name,
+                Value=value,
+                Type='String',
+                Overwrite=True
+            )
