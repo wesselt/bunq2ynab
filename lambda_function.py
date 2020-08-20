a10d0bf487c64231ad97a6adaabf57349299a67e
Javy de Koning <javy@amazon.com>
2020-08-20 09:51:37 +0200

    Fixing role, adding stateJson, moving config inside of function handler

--- lambda_function.py
+++ lambda_function.py
@@ -2,10 +2,8 @@ from lib.sync import Sync
 from lib.config import config
 
 
-config.load()
-
-
 def lambda_handler(event, context):
+    config.load()
     sync = Sync()
     sync.populate()
     sync.synchronize()
