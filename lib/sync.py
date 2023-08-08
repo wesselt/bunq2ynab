96e1d41e469623f85d12ba8462270cd808046caf
wesselt <wesselt@aule.net>
2023-08-08 11:03:58 +0200

    Use UTC dates, ignore future YNAB dates

--- lib/sync.py
+++ lib/sync.py
@@ -158,7 +158,7 @@ class Sync:
         elif get_start:
             start_dt = get_start
         else:
-            dt = datetime.datetime.now() - datetime.timedelta(days=35)
+            dt = datetime.datetime.utcnow() - datetime.timedelta(days=35)
             start_dt = dt.strftime("%Y-%m-%d")
 
         log.info("Reading ynab transactions from {}...".format(start_dt))
@@ -169,6 +169,13 @@ class Sync:
         if not get_all and not get_start:
             # Push start date back to latest uncleared YNAB entry
             start_dt = get_last_transaction_date(transactions)
+            today_dt = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+            if today_dt < start_dt:
+                # Manual clear combined with timezone can put a transaction
+                # in the UTC future
+                # https://github.com/wesselt/bunq2ynab/issues/46
+                log.warn(f"Last YNAB transaction {start_dt} is in the future")
+                start_dt = today_dt
 
         log.info("Reading bunq payments from {}...".format(start_dt))
         payments = bunq_api.get_payments(syncpair["bunq_user_id"],
