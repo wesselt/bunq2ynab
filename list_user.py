737128871d215796d34ad25008188a39f3005a41
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-08-29 21:09:07 +0200

    first working version

--- list_user.py
+++ list_user.py
@@ -8,20 +8,24 @@ config.load()
 
 
 def print_accounts(userid):
-    method = 'v1/user/{0}/monetary-account'.format(userid)
+    method = "v1/user/{0}/monetary-account".format(userid)
     for a in bunq.get(method):
         for k, v in a.items():
-            print("  {0:50.50}  {1:10,} {2:3}  {3:10} {4:25.25} {5}".format(
-                v["description"],
-                Decimal(v["balance"]["value"]),
-                v["balance"]["currency"],
-                v["status"],
-                k,
-                v["id"]))
+            print(
+                "  {0:50.50}  {1:10,} {2:3}  {3:10} {4:25.25} {5}".format(
+                    v["description"],
+                    Decimal(v["balance"]["value"]),
+                    v["balance"]["currency"],
+                    v["status"],
+                    k,
+                    v["id"],
+                )
+            )
 
 
-users = bunq.get('v1/user')
+users = bunq.get("v1/user")
+
 for u in users:
     for k, v in u.items():
-        print('{0} "{1}" ({2})'.format(k, v["display_name"], v["id"]))
+        print('{0} "{1}" ({2})'.format(k, v.get("display_name", ""), v["id"]))
         print_accounts(v["id"])
